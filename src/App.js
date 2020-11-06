359f87a15c4b0b7806ff7b905043c227299c5f0a
Shubhamsg7 <shubhamkumar8028@gmail.com>
2020-11-07 00:55:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import ReplayIcon from '@material-ui/icons/Replay';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 
-function App() {
+
+const App = () => {
+
+  const [num, setNum] = useState(0);
+
+  const incNum = () => {
+    setNum(num + 1);
+  };
+
+  const reset = () => {
+    setNum(0);
+  };
+  
+  const decNum = () => {
+    setNum(num - 1);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Card className="card">
+          <h1>{num}</h1>
+          <div className="btn-container">
+                <Button className="btn decrease" onClick={decNum}><RemoveIcon /></Button>
+                <Button className="btn reset" onClick={reset}><ReplayIcon /></Button>
+                <Button className="btn increase" onClick={incNum}><AddIcon /></Button>
+            </div>
+        </Card>
+      </div>
+    </>
   );
 }
 
